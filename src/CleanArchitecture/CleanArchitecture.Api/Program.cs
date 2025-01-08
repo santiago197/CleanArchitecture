8c74d1fdcca77965f4ab746e991a3f799eef95b4
Santiago Rodriguez <santinacional123.sr@gmail.com>
2025-01-08 09:33:51 -0500

    Controllers

---
+++ src/CleanArchitecture/CleanArchitecture.Api/Program.cs
@@ -0,0 +1,25 @@
+using CleanArchitecture.Application;
+using CleanArchitecture.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastrcture(builder.Configuration);
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
